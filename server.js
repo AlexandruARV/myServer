73070d8a6c1bd1b75d7331d0f837844c311a1a41
AlexandruARV <162844302+AlexandruARV@users.noreply.github.com>
2024-03-10 16:45:06 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const http = require("http");
 
+const data = { lala: 2, dada: 3 };
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("hello Worldd");
+  res.end(data);
 });
 
 const PORT = process.env.PORT || 3000;
