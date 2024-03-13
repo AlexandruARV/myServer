1109283bb0e4c688d0f484afa67c3c0b99d046c8
AlexandruARV <162844302+AlexandruARV@users.noreply.github.com>
2024-03-13 11:59:12 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ const app = express();
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
