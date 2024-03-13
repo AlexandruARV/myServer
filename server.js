d0bfeb568cf009cd9c753b587e2bf4112a4dbf13
AlexandruARV <162844302+AlexandruARV@users.noreply.github.com>
2024-03-13 12:04:06 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ console.log(JSON.stringify(data));
 
 const app = express();
 
+const sotreData = [];
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
@@ -32,6 +34,13 @@ app.use((req, res, next) => {
 
 app.get("/", (req, res) => {
   res.status(200).json(data);
+});
+
+app.post("/formPost", (req, res) => {
+  res.json(sotreData);
+  console.log(req.body);
+  sotreData.push(req.body);
+  console.log(sotreData);
 });
 
 const PORT = process.env.PORT || 3000;
