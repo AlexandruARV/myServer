19de672bf1f2a1ffb7288f74e93a33a3aea6b60c
AlexandruARV <162844302+AlexandruARV@users.noreply.github.com>
2024-03-13 11:53:36 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,16 +1,38 @@
-const http = require("http");
+// const http = require("http");
+
+// const data = { lala: 2, dada: 3 };
+
+// console.log(JSON.stringify(data));
+
+// const server = http.createServer((req, res) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "*");
+//   res.writeHead(200, { "Content-Type": "application/json" });
+//   res.end(JSON.stringify(data));
+// });
+
+// const PORT = process.env.PORT || 3000;
+
+// server.listen(PORT, () => console.log(`server is running on port 3000`));
+
+const express = require("express");
 
 const data = { lala: 2, dada: 3 };
 
 console.log(JSON.stringify(data));
 
-const server = http.createServer((req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "*");
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(data));
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "");
+  res.setHeader("Access-Control-Allow-Methods", "");
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.status(200).json(data);
 });
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log(`server is running on port 3000`));
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
